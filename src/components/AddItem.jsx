58d73aebea433a9a6db5ce704f47680fdc2cf104
Addison <addison.frei@icloud.com>
2022-07-22 15:05:09 -0400

    useState added to AddItem

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 
 function AddItem() {
+  const [formData, setFormData] = useState({
+      "name": "",
+      "image": "",
+      "price": "$"
+  })
+  console.log(formData)
+
+  function handleChange(e) {
+    setFormData({...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
   return (
     <Box
     component="form"
     sx={{
       '& .MuiTextField-root': { m: 1, width: '25ch' },
     }}
-    noValidate
-    autoComplete="off"
+    // noValidate
+    // autoComplete="off"
   >
     <div>
       <br />
@@ -19,19 +32,25 @@ function AddItem() {
         required
         id="outlined-required"
         label="Name"
+        name="name"
         defaultValue="Item Name"
+        onChange={handleChange}
       />
       <TextField
         required
         id="outlined-required"
-        label="Image"
-        value="Image URL"
+        label='Image'
+        name='image'
+        value={formData.image}
+        onChange={handleChange}
       />
       <TextField
         required
         id="outlined-required"
         label="Item Price"
+        name='price'
         defaultValue="49.99"
+        onChange={handleChange}
       />
       <br />
       <Button 
