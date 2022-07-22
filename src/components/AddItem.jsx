a60c8460b35e5e7e11691aa1815d4fc496aecbce
Addison <addison.frei@icloud.com>
2022-07-22 11:53:18 -0400

    adding form to AddItem component

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function AddItem() {
   return (
-    <h1>Add Item Form</h1>
+    <Box
+    component="form"
+    sx={{
+      '& .MuiTextField-root': { m: 1, width: '25ch' },
+    }}
+    noValidate
+    autoComplete="off"
+  >
+    <div>
+      <br />
+      <TextField
+        required
+        id="outlined-required"
+        label="Name"
+        defaultValue="Item Name"
+      />
+      <TextField
+        required
+        id="outlined-required"
+        label="Image"
+        defaultValue="Image URL"
+      />
+      <TextField
+        required
+        id="outlined-required"
+        label="Item Price"
+        defaultValue="49.99"
+      />
+      <br />
+      <Button size="medium" onClick={() => console.log("Clicked")}>Submit</Button>
+
+      
+    
+    </div>
+  </Box>
   )
 }
 
